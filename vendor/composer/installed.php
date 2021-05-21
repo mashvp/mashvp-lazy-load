e46816d24354655285f582a607ead40203190176
Guillaume de Matos <gdematos@mashvp.com>
2021-05-21 14:24:50 +0200

    Include vendors in Git
    * Vendors would be missing after installing through TGMPA

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'kornrunner/blurhash' => 
+    array (
+      'pretty_version' => 'v1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7dde9c0c7ee7e688dc79fb7770ee110ed308ffc7',
+    ),
+  ),
+);
